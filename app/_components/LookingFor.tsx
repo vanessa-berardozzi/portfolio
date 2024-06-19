2705c8dcbbfc89ab61b74042fd8be3fe6808e298
vanessa berardozzi <ness.berardozzi@gmail.com>
2024-06-19 03:33:04 +0200

    Chore : Update Skill section

--- app/_components/LookingFor.tsx
+++ app/_components/LookingFor.tsx
@@ -4,22 +4,22 @@ import type style from 'styled-jsx/style';
 
 export const LookingFor = () => {
   return (
-    <Section className=' flex max-md:flex-col items-start'>
-      <div className=' flex flex-col gap-4'></div>
+    <Section className='flex max-md:flex-col justify-center items-center'>
+      <div className='flex flex-col '></div>
 
-      <Card className='w-full p-4 flex flex-col gap-2'>
-        <div className='flex flex-col gap-4'>
-          <h2 className='pb-2 text-3xl font-semibold tracking-tight first:mt-0'>
+      <Card className='w-full p-2 flex flex-col gap-1 items-center'>
+        <div className='flex flex-col gap-3'>
+          <h2 className='pb-2 text-3xl font-caption font-semibold tracking-tight first:mt-0 text-center'>
             Ma recherche
           </h2>
-          <div className='text-base '>
-            <p>
-              👀 Contrat d'<span className=' bg-gradient-to-r from-[#ddee65] to-[#f4dba7] 7 inline-block text-transparent bg-clip-text '>
+          <div className='text-base text-center'>
+            <p className='mb-1'>
+              👀 Contrat d'<span className='bg-gradient-to-r from-[#ddee65] to-[#f1dba7] 7 inline-block text-transparent bg-clip-text'>
                 apprentissage✨
               </span>
             </p>
-            <p>⏱️Durée: entre 15 et 23 mois</p>
-            <p>
+            <p className='mb-1'>⏱️Durée: entre 15 et 23 mois</p>
+            <p className='mb-1'>
               📍 Localité : Région Lyonnaise, Rhône-Alpes , France entière
               possible selon opportunité
             </p>
--- app/_components/Status.tsx
+++ app/_components/Status.tsx
@@ -1,18 +1,11 @@
 /* eslint-disable @next/next/no-img-element */
 
-
 import { Card } from '@/components/ui/card';
-import { ContactCard } from './ContactCards';
 import React from 'react';
 import { Section } from './Section';
 import { Projects, type ProjectsProps } from './Projects';
 import { Code, FileQuestion, MusicIcon } from 'lucide-react';
 import { Work, type WorkProps } from './Works';
-
-
-import type { url } from 'inspector';
-import type image from 'next/image';
-import type { title } from 'process';
 
 export const Status = () => {
 	return (
@@ -32,8 +25,10 @@ export const Status = () => {
 			</div>
 			<div className='flex-[2] w-full flex flex-col  gap-4 '>
 				<Card className='p-4 flex-1'>
-					<p className='text-lg  text-muted-foreground'>Expériences professionnelles </p>
-					<div className='flex flex-col gap-4' >
+					<p className='text-lg  text-muted-foreground'>
+						Expériences professionnelles{' '}
+					</p>
+					<div className='flex flex-col gap-4'>
 						{WORKS.map((work, index) => (
 							<Work
 								key={index}
@@ -43,8 +38,7 @@ export const Status = () => {
 					</div>
 				</Card>
 
-				
-					{/*
+				{/*
 					<Card
 						className='p-4 flex-1 flex flex-col gap-2'
 						flex-1
@@ -59,7 +53,6 @@ export const Status = () => {
 						/>
 					</Card>
 					*/}
-					
 			</div>
 		</Section>
 	);
@@ -85,8 +78,7 @@ const WORKS: WorkProps[] = [
 ];
 
 const PROJECTS: ProjectsProps[] = [
-
-{
+	{
 		Logo: FileQuestion,
 		title: 'Game-O-Quiz',
 		description: 'Back-End Developer',
@@ -95,12 +87,10 @@ const PROJECTS: ProjectsProps[] = [
 
 	{
 		Logo: MusicIcon,
-		title: 'Spotify Playlist ( comming soon ... )',
+		title: 'Spotify Playlist ( coming soon ... )',
 		description: 'Frontend Developer',
 		url: '/',
 	},
-
-
 ];
 
 const CONTACTS = [
@@ -119,6 +109,6 @@ const CONTACTS = [
 			'https://media.licdn.com/dms/image/C5603AQFHZV4aKgBRDg/profile-displayphoto-shrink_800_800/0/1569423474257?e=1723680000&v=beta&t=Dks0V7bDJzRhEIFomStGZo8uSCnKGG4cPz9S45UnERE',
 		mediumImage:
 			'https://play-lh.googleusercontent.com/KSuaRLiI_FlDP8cM4MzJ23ml3og5Hxb9AapaGTMZ2GgR103mvJ3AAnoOFz1yheeQBBI',
-		name: "bla"
+		name: 'bla',
 	},
 ];
